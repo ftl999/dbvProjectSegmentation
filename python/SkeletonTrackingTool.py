6d804530834ed736c4c9b8f5fd41ed653cbdb712
Felix Lehner <f.lehner@tu-bs.de>
2021-01-18 19:11:04 +0100

    created pipe till tracking

--- python/SkeletonTrackingTool.py
+++ python/SkeletonTrackingTool.py
@@ -1,3 +1,4 @@
+from PointTracker import PointTracker
 from VideoHelper import FileVideoInputStage
 from GUI import GUI
 from ProcessingPipe import ProcessingPipe
@@ -9,5 +10,5 @@ if __name__ == "__main__":
     ProcessingPipe.addStage("Segmentation", StaticImageInputStage(cv2.imread(os.path.join(os.path.dirname(sys.argv[0]), "Mask.png"))))
     ProcessingPipe.addStage("PointsExtraction", PointExtractor(), ["original", "Segmentation"])
     ProcessingPipe.addStage("VideoIn", FileVideoInputStage())
-    ProcessingPipe.addStage("PointsTracking", None, ["PointsExtraction", "VideoIn"])
+    ProcessingPipe.addStage("PointsTracking", PointTracker(), ["PointsExtraction", "VideoIn"])
     gui = GUI()
